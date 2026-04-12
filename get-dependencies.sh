925e650f76f0ec31214845e77979558e0af1ff50
Link4Electronics <link4electronics@gmail.com>
2026-04-12 01:12:06 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm primslauncher
+pacman -Syu --noconfirm prismlauncher
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
