d788b7a6830c67a5826c5a32fc03a717da27b952
Link4Electronics <link4electronics@gmail.com>
2026-04-12 01:09:30 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -3,16 +3,19 @@
 set -eu
 
 ARCH=$(uname -m)
-VERSION=$(pacman -Q PACKAGENAME | awk '{print $2; exit}') # example command to get version of application here
+VERSION=$(pacman -Q prismlauncher | awk '{print $2; exit}') # example command to get version of application here
 export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=PATH_OR_URL_TO_ICON
-export DESKTOP=PATH_OR_URL_TO_DESKTOP_ENTRY
+export ICON=/usr/share/icons/hicolor/scalable/apps/org.prismlauncher.PrismLauncher.svg
+export DESKTOP=/usr/share/applications/org.prismlauncher.PrismLauncher.desktop
+export DEPLOY_QT=1
+export QT_DIR=qt6
+export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /PATH/TO/BINARY_AND_LIBRARIES_HERE
+quick-sharun /usr/bin/prismlauncher
 
 # Additional changes can be done in between here
 
